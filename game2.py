a78a9bbb99e2f77ff67aeacdc1dae29f46158354
rishika611 <khushboolahoti21@gmail.com>
2025-05-31 11:37:56 +0400

    Add files via upload

---
+++ game2.py
@@ -0,0 +1,25 @@
+import math
+import random
+import pygame
+
+SCREEN_WIDTH=800
+SCREEN_HEIGHT=500
+pygame.init()
+screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+background=pygame.image.load('background.png')
+running=True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+
+
+
+
+
+
+
+
+
+
+pygame.display.update()
+
